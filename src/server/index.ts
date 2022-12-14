fe384c70dd2efb3b57663de054caa8cef5e54740
Mopcho <valeri_petkov01@abv.bg>
2022-12-14 16:47:33 +0200

    Cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -21,7 +21,7 @@ const app: Express = express();
 // ------------ CORS -------------
 app.use(cors({
 	credentials: true,
-	origin: 'http://localhost:3000'
+	origin: '*'
 }));
 
 // ----------- BODY PARSER --------------
